7676001303f4d50e0026dac8af0a6a1910190f81
Andre-Ovien <andreovien571@gmail.com>
2025-09-11 09:41:39 +0100

    updates

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.blog, name="blog"),
+    path('events/', views.event, name="event"),
+    path('posts-detail/<int:pk>/', views.blog_detail, name="post_detail"),
     path('blog-post/', views.ListCreateBlogApi.as_view(),),
     path('blog-detail/<pk>/', views.BlogDetailApi.as_view(),),
+    path('blog/<int:post_id>/comments/', views.CommentListCreateApi.as_view(), name='post-comments'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, AllowAny
-from .models import Blog
-from .serializers import BlogSerializer
+from .models import Blog, Comment
+from .serializers import BlogSerializer, CommentSerializer
 
 # Create your views here.
 
@@ -34,3 +34,29 @@ class BlogDetailApi(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method in ['PUT','PATCH','DELETE']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
+    
+
+def blog_detail(request, pk):
+    context = {
+        "post_id": pk
+        }
+    return render(request, 'blog_detail.html', context)
+
+
+class CommentListCreateApi(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        serializer.save(post_id=post_id)
+
+
+def event(request):
+    context ={
+
+    }
+    return render(request, 'event.html',context)
