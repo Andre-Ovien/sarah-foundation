3869ce793ba37cee768b9316d6a295d84a4ebc28
Andre-Ovien <andreovien571@gmail.com>
2025-09-20 00:06:37 +0100

    minor update

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from .models import Blog, Comment
 
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'name',
+            'email',
+            'comment',
+            'comment_date',
+        )
+
 class BlogSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Blog
         fields = (
@@ -13,13 +27,5 @@ class BlogSerializer(serializers.ModelSerializer):
             'image',
             'publish_date',
             'update_date',
-        )
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = (
-            'name',
-            'email',
-            'comment',
+            'comments',
         )
