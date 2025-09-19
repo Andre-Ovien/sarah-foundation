80635a86b4fd5784ea02d45cf02ed21778c0ff4c
Andre-Ovien <andreovien571@gmail.com>
2025-09-20 00:06:54 +0100

    update

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.blog, name="blog"),
-    path('events/', views.event, name="event"),
     path('posts-detail/<int:pk>/', views.blog_detail, name="post_detail"),
     path('blog-post/', views.ListCreateBlogApi.as_view(),),
     path('blog-detail/<pk>/', views.BlogDetailApi.as_view(),),
