f0052cd824c61abf4587571f767500fa0ba06fc4
Andre-Ovien <andreovien571@gmail.com>
2025-09-21 12:11:40 +0100

    views for events

--- Events/views.py
+++ Events/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from rest_framework.viewsets import generics 
 from .models import Event
-from .serializers import EventSerializer
+from .serializers import EventSerializer, EventDetailSerializer
 from rest_framework.permissions import AllowAny, IsAdminUser
 
 # Create your views here.
 
 
 class EventListCreateApiView(generics.ListCreateAPIView):
-    queryset = Event.objects.all()
+    queryset = Event.objects.all().order_by('-date')
     serializer_class = EventSerializer
 
     def get_permissions(self):
@@ -23,3 +23,30 @@ def events(request):
 
     }
     return render(request, 'event.html',context)
+
+class EventRetrieveUpdateDeleteApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventDetailSerializer
+    permission_classes = [AllowAny]
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+#    def get_queryset(self):
+        return super().get_queryset()
+
+
+def event_details(request,pk):
+    context = {
+
+    }
+    return render(request, 'event_details.html', context)
+
+
+#class EventListApiView(generics.ListAPIView):
+#    queryset = Event.objects.all()
+#    serializer_class = EventDetailSerializer
+#    permission_classes = [AllowAny]
