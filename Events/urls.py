f9faeeeb7b1028b6539bdb3a0b6d3f62f0bc4cf3
Andre-Ovien <andreovien571@gmail.com>
2025-09-21 12:10:53 +0100

    endpoints

--- Events/urls.py
+++ Events/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.EventListCreateApiView.as_view()),
-    path('events/', views.events, name="event"),
+    path('list/', views.events, name="event"),
+    path('details/<pk>/', views.event_details, name="event_details"),
+    path('events-details/<pk>/', views.EventRetrieveUpdateDeleteApiView.as_view(), name="event-detail"),
+#    path('events/', views.EventListApiView.as_view()),
 ]
