ccb1e1e5af5d72a8fa3788d8e1c200f75e183093
Andre-Ovien <andreovien571@gmail.com>
2025-09-21 12:10:11 +0100

    serializers for Events

--- Events/serializers.py
+++ Events/serializers.py
@@ -1,14 +1,58 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, RegisterEvent
 
 
 class EventSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(write_only=True)
+    image = serializers.ImageField(write_only=True)
     class Meta:
         model = Event
         fields = (
-            'name',
-            'address',
+            'id',
+            'title',
             'text',
+            'location',
             'description',
             'date',
+            'start_time',
+            'end_time',
+            'image',
+        )
+
+
+class RegisterEventSerializer(serializers.ModelSerializer):
+    event = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = RegisterEvent
+        fields = (
+            'full_name',
+            'email',
+            'phone',
+            'status',
+        )
+
+
+
+
+class EventDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = (
+            'id',
+            'title',
+            'text',
+            'location',
+            'description',
+            'date',
+            'start_time',
+            'end_time',
+            'image',
+            'highlight_1',
+            'highlight_2',
+            'highlight_3',
+            'highlight_4',
+            'highlight_5',
+            'image_1',
+            'image_2',
+            'image_3',
         )
