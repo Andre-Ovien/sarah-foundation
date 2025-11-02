66dfbd52abff3e58949cc14b13662bb52808035f
Andre-Ovien <andreovien571@gmail.com>
2025-11-02 08:19:24 +0100

    update

--- SarahFoundation/settings.py
+++ SarahFoundation/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'SarahFoundation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 
-#DATABASES = {
-#    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-#}
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
